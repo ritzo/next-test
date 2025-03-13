634719bb4aac9f9a359410067c7b3523a1d386ef
Matias Risso <rissojuanm@gmail.com>
2025-03-13 20:35:34 -0300

    Deploy fix part 2

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,4 +2,4 @@ import { Inter } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
 
-export const lusitana = Inter({ weight: '700' });
+export const lusitana = Inter({ weight: '700', subsets: ['latin'] });
